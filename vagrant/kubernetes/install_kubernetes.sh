b77e9ac03d6e72b41bb2cc1c40070bd42e0c8540
Emmanuel Girin <esgirin@gmail.com>
2022-09-20 18:42:04 +0200

    Fix ip

--- vagrant/kubernetes/install_kubernetes.sh
+++ vagrant/kubernetes/install_kubernetes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 now=$(ip -f inet addr show eth0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
+
 yum -y update
 yum -y install epel-release
 
@@ -19,6 +20,9 @@ then
         echo "You need to be root to use kubectl in $(ip -f inet addr show eth0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') VM (run 'sudo su -' to become root and then use kubectl as you want)"
         echo "###################################################"
 else
+		NOWORKERS=$2
+		now=$(ip -f inet addr show ens33 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
+		now=$(echo $now | awk -F. -v a="$NOWORKERS" '{print $1"."$2"."$3"."($4-a)}')
         ansible-playbook install_kubernetes.yml --extra-vars "kubernetes_role=$1 kubernetes_apiserver_advertise_address=$now kubernetes_join_command='kubeadm join {{ kubernetes_apiserver_advertise_address }}:6443 --ignore-preflight-errors=all --token={{ token }}  --discovery-token-unsafe-skip-ca-verification'"
         echo "For this Stack, you will use $(ip -f inet addr show eth0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
 fi
